095f96a0eefcd09f17e5b0c60882515e75d9856b
Aleeza Zahra <127841293+aleezazahra@users.noreply.github.com>
2026-05-29 00:53:02 +0500

    add

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        gwendolyn: ['Gwendolyn', 'cursive'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
